ef9f4448e582e56f77eec9ff2c84b1bdcf1e7912
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 23:30:05 +0530

    submit

---
+++ Day 6/Icommunications/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces_concept
+{
+
+    interface IACT
+    {
+        void network();
+    }
+    class ABC : IWork, IACT, ICommunications
+    {
+        public void add()
+        {
+            Console.WriteLine("I am from IWork Interface");
+        }
+
+        public int calc()
+        {
+            int result = 55;
+            return result;
+        }
+        public void CNN()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void network()
+        {
+            Console.WriteLine("I am from IACT network");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ABC obj = new ABC();
+            obj.add();
+            obj.calc();
+            obj.network();
+            XYZ objx = new XYZ();
+            objx.calc();
+            objx.add();
+
+        }
+    }
+}
---
+++ Day 6/Icommunications/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces_concept
+{
+    class XYZ : IWork
+    {
+
+        public void add()
+        {
+            Console.WriteLine("I am From class XYZ");
+        }
+
+        public int calc()
+        {
+            int sum;
+            sum = 10;
+            return sum;
+        }
+    }
+}
