1c6acab25ecb3fa76c452a20bb8e3724d8d5e7b6
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 23:44:27 +0530

    submit

---
+++ Day 7/PolymorphismDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismDemo
+{
+    class Program
+    {
+        static double Area(double l, double w)
+        {
+            return l * w;
+        }
+        static double Area(double R)
+        {
+            const double PI = 3.14159;
+            return R * R * PI;
+        }
+        static void Main(string[] args)
+        {
+            double length, width;
+            double Radius;
+            Console.WriteLine("Enter the sides of Rectangle");
+
+            Console.WriteLine("Length : ");
+            length = double.Parse(Console.ReadLine());
+            Console.WriteLine("width :");
+            width = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n Area of Rectangle");
+            Console.WriteLine(Area(length, width));
+
+            //Area
+            Console.WriteLine("Enter the Radius");
+            Radius = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n Area Of Circle");
+            Console.WriteLine(Area(Radius));
+            Console.ReadLine();
+
+        }
+    }
+}
