78e37f3f4fba975727f5df1eec1987890b8a5816
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:26:23 +0530

    submit

---
+++ Day 11/NonGenericStack/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating 
+            Stack stack = new Stack();
+
+            //Adding
+            stack.Push("David");
+            stack.Push("Emmy");
+            stack.Push("Jack");
+            stack.Push("Emma");
+
+            //Displaying
+            foreach (var st in stack)
+            {
+                Console.WriteLine(st);
+            }
+            Console.WriteLine(stack.Peek());
+
+            //REmoving
+            Console.WriteLine("Sequence pf removing");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
