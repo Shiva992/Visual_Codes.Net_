bc602054856eda93819a86539388185e54845a4d
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 21:42:52 +0530

    submit

---
+++ Day 2/PalindromeAndArmstrong/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Palindrome
+            //Console.WriteLine("Enter the String");
+            //string s = Console.ReadLine();
+            //int n = s.Length,i;
+            //for(i = 0; i <n / 2; i++)
+            //{
+            //    if (s[i] != s[n - i - 1])
+            //    {
+            //        Console.WriteLine("Not Palindrome");
+            //        break;
+            //    }
+            //}
+            //if(i==n/2)
+            //    Console.WriteLine("Palindrome");
+
+
+            //Armstrong Number
+            //Console.WriteLine("Enter the String");
+            //double sum=0;
+            //int n = int.Parse(Console.ReadLine());
+            //int res = n;
+            //int i = 0;
+            //while (n > 0)
+            //{
+
+            //    i++;
+            //    n /= 10;
+            //}
+            //n = res;
+            //while (n > 0)
+            //{
+            //    int k = n % 10;
+            //    sum+=Math.Pow(k,i);
+            //    n /= 10;
+
+            //}
+            //if (res == sum)
+            //{
+            //    Console.WriteLine("Armstrong");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Armstrong");
+            //}
+
+
+
+        }
+    }
+}
