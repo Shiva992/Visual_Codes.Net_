d2408fd3de971a487485d21a2102245a04417f70
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 22:08:08 +0530

    submit

---
+++ Day 5/Laptop/inheritance/Inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class plane1
+    {
+        public virtual void status()
+        {
+            Console.WriteLine("Flying plane 1");
+        }
+    }
+    class plane2 : plane1
+    {
+        public override void status()
+        {
+            Console.WriteLine("Plane 2");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Plane3 p = new Plane3();
+            p.status();
+        }
+    }
+}
