951d472db9f4bc9d9eb48748c709642159e7136b
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:24:33 +0530

    submit

---
+++ Day 11/NonGenericQueue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating
+            Queue queue = new Queue();
+            //Adding
+            queue.Enqueue("Rishabh");
+            queue.Enqueue("Chirag");
+            queue.Enqueue("Amit");
+            Console.WriteLine("Count: {0} ", queue.Count);
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Contains
+            Console.WriteLine("checking for Amit: " + queue.Contains("Amit"));
+
+
+            //Peek
+            Console.WriteLine("Peek :" + queue.Peek());
+            //Removing
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.ReadKey();
+        }
+    }
+}
