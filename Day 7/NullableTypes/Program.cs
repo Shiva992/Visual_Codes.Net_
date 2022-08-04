ce4e3b4a5dfb0d2b236546cc12c368d1f64d6d57
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 23:40:06 +0530

    submit

---
+++ Day 7/NullableTypes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullabletypes
+
+{
+    class Myclass
+    {
+        public Nullable<int> i;
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //In C3, if we try to assign a null value to the value type variables,
+            //then we will get a compile time error.
+
+            //try this
+
+            //int? g = null /// its the right way for assigning the nullable type
+            //int g =null /// this will give error.
+            //bool y  =true;
+            // console.writeline("")
+            Myclass cls = new Myclass();
+            if (cls.i == null)
+            {
+                Console.WriteLine("Null");
+
+            }
+            else
+            {
+                Console.WriteLine(cls.i.Value);
+
+            }
+            //=============================================
+
+            int? a = null;
+            if (a.HasValue)
+            {
+                Console.WriteLine("x = {0}", a.Value);
+
+            }
+            else
+            {
+                Console.WriteLine("Value is Empty");
+            }
+            Console.ReadLine();
+
+            int? x = null;
+            Nullable<int> z;
+            int y = x ?? 0;
+            Console.WriteLine("y = {0}", y);
+            Console.ReadLine();
+        }
+    }
+}
