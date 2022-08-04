685c4f5dfc712a8120753259fc84f111f7c188e2
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 01:03:05 +0530

    submit

---
+++ Day 2/constAndReadonly/ReadConst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            const string name = "rwal";
+            const string location = "Bangalore";
+            const int age = 32;
+
+
+            //string name = "Rahul";
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Location: {0}", location);
+            Console.WriteLine("Age: {0}", age);
+            Console.ReadLine();
+        }
+    }
+}
