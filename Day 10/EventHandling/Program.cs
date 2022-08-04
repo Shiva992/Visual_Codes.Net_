25373d675041b06c803add6e24dd059d47360aec
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:12:59 +0530

    submit

---
+++ Day 10/EventHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandlingDemo
+{
+    // This is Subscriber Class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddTwoNumbers a = new AddTwoNumbers();
+            a.ev_OddNumber += new AddTwoNumbers.dg_OddNumber(EventMessage);
+            a.ev_OddNumber += new AddTwoNumbers.dg_OddNumber(AlertMessage);
+            a.Add();
+            Console.Read();
+        }
+
+        static void EventMessage()
+        {
+            Console.WriteLine("Event Executed : This is Odd Number");
+        }
+        static void AlertMessage()
+        {
+            Console.WriteLine("Event Executed : Event Executed");
+        }
+    }
+
+    // This is Publisher Class
+    class
+}
