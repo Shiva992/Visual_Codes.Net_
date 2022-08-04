2b7d1a052b030b2c99602c88a6b230e3d7fb91be
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:37:03 +0530

    submit

---
+++ Student Segregation Task/Student segregation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDetailsFilesSegregated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentManagementIO studentManagementIO = new StudentManagementIO();
+            Boolean toExit = true;
+            do
+            {
+                switch (studentManagementIO.Menu())
+                {
+                    case 1:
+                        {
+                            studentManagementIO.AddStudent();
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            studentManagementIO.DisplayAll();
+                            break;
+                        }
+                    case 3:
+                        {
+                            studentManagementIO.UpdateStudent();
+                            break;
+                        }
+                    case 4:
+                        {
+                            studentManagementIO.DeleteStudent();
+                            break;
+                        }
+                    case 0:
+                        {
+                            toExit = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Enter the valid option");
+                            break;
+                        }
+                }
+            } while (toExit);
+        }
+    }
+}
