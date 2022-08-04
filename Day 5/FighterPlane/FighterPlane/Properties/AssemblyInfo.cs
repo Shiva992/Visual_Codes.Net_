abb1e54e6ba6d5c0637acac876c28a410772b364
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 22:15:29 +0530

    Submit

---
+++ Day 5/FighterPlane/FighterPlane/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FighterPlane")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FighterPlane")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d8d8e74-e702-42ab-8d99-5638e335e38a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day 5/cargoPlane/CargoPlane/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanesInheritance
+{
+    class CargoPlane : Plane
+    {
+        internal override void TakeOff()
+        {
+            Console.WriteLine("Cargoplane wants long run way to take off");
+
+        }
+        public override void Fly()
+        {
+            Console.WriteLine(" Cargoplane fly with low speed");
+
+        }
+        public override void Land()
+        {
+            Console.WriteLine("Cargoplane to land wants long run way");
+
+        }
+    }
+}
