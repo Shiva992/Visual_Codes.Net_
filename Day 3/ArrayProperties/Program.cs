ebccd7e419f3a1335d7c6f5c812a709bf53d2c32
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 21:48:10 +0530

    submit

---
+++ Day 3/ArrayProperties/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sum = 0,k=0;
+            //int[] arr = { 1, 2, 1, 4 ,2,6,9,9,4};
+            //Console.WriteLine("Reverse Array");
+            //for (int i = arr.Length - 1; i >= 0; i--)
+            //{
+
+            //    Console.WriteLine(arr[i]);
+            //}
+            //Console.WriteLine("Copy Array");
+            //int[] b= new int[arr.Length];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    b[i] = arr[i];
+            //    Console.WriteLine(b[i]);
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //    sum += arr[i];
+            //Console.WriteLine("Sum is {0}",sum);
+            //for(int i = 0; i < arr.Length; i++)
+            //{
+            //    for(int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if (arr[i] == arr[j])
+            //        {
+            //            k++;
+
+
+            //        }
+            //    }
+
+            //}
+            //Console.WriteLine("Duplicate is {0}", k);
+
+            //Array.Reverse(b);
+            //Array.Sort(arr);
+            //for (int i = 0; i < arr.Length; i++)
+
+            //    Console.WriteLine(arr[i]);
+
+
+
+            Program p = new Program();
+
+            // UNCOMMENT THE LINE YOU WANT TO RUN
+
+            // p.SingleDimensionalArray();
+            // p.MultiDimensionalArray();
+            // p.JaggedArrays();
+            // p.ArraysSample();
+
+            // p.ArrayClassProperties();
+
+            // p.ArrayCreateInstanceSample();
+
+            //  p.SortAndSearchArray();
+
+            // p.GetSetArrayValues();
+
+            // p.ReverseArray();
+
+            // p.ClearArray();
+
+            // p.CopyArray();
+
+            // p.CloneArray(); 
+
+
+
+        }
