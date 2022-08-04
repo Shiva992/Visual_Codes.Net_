58ca9ba13731cc5568e15d5c7180ab9367928cbd
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 23:32:47 +0530

    submit

---
+++ Day 6/PartialEmployee/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartialEmployee emp = new PartialEmployee();
+            emp.FirstName = "Rishabh";
+            emp.LastName = "Dora";
+            emp.Salary = 100000;
+            emp.Gender = "Male";
+            emp.DisplayFullName();
+            emp.DisplayEmployeeDetails();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day 6/PartialEmployee/PartialEmployee 2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public partial class PartialEmployee
+    {
+        //Private field members
+        string _firstName;
+        string _lastName;
+        double _salary;
+        string _gender;
+        //properties
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public double Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+
+        }
+        public string Gender
+        {
+            get { return _gender; }
+            set
+            {
+                _gender = value;
+            }
+        }
+    }
+}
