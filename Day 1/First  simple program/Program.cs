dbb9f71228449df91e4229a05628a6ffc3d02f49
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 21:25:47 +0530

    submit

---
+++ Day 1/First  simple program/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("strat");
+            const string s = "luckyj";
+            const int num = 145;
+
+            Console.WriteLine("Name is {0}", s);
+            Console.WriteLine("Number is {0}", num);
+            Console.ReadLine();
+
+            //byte a = 75;
+            //string abb= "lucky";
+            //float k = 5.5f;
+
+            //double d = 0.123456789d;
+            ////Console.WriteLine(d);
+            //int[] arr = { 1, 2, 3, 4, 5 };
+            ////Console.WriteLine(arr[4]);
+
+            //float i = float.Parse(Console.ReadLine());             
+            //int x=Convert.ToInt32(i);
+            /*Console.WriteLine(x);*/
+
+            //fact
+            //int res = 1;
+            //int a = 5;
+            //for (int i = 1; i <= a; i++)
+            //    res = res * i;
+            //Console.WriteLine(res);
+
+            //fibo
+            //{
+            //    int a = 5;
+            //    int f = 0, s = 1, res;
+            //    Console.WriteLine(f);
+            //    Console.WriteLine(s);
+            //    for (int i = 2; i < a; i++)
+            //    {
+            //        res = f + s;
+            //        f = s;
+            //        s = res;
+            //        Console.WriteLine(res);
+            //    }
+
+            //}
+        }
+
+
+
+    }
+}
