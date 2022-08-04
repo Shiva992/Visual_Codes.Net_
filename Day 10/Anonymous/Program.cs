ce4305fd1d7b1bbc2f84d0936cf6a6c40747373e
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:14:17 +0530

    submit

---
+++ Day 10/Anonymous/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate void operation();
+        static void Main(string[] args)
+        {
+            operation obj = delegate
+            {
+                Console.WriteLine(" I am Anonymous method");
+            };
+            obj();
+            Console.ReadLine();
+        }
+    }
+}
