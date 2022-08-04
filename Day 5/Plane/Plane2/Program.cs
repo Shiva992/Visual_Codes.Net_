0986b9491a1f513e36cb7e63b0a8ccb05331581b
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 22:18:28 +0530

    submit

---
+++ Day 5/Plane/Plane2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PlanesInheritance
+{
+    class PlaneApp
+    {
+        static void Main(string[] args)
+        {
+            CargoPlane cp = new CargoPlane();
+            FighterPalne fp = new FighterPalne();
+            Airport ap = new Airport();
+            ap.AcceptPlane(cp);
+
+        }
+    }
+}
