90088a05e56caf7f1fcb2e5109a1d848fb3e7bfb
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:05:18 +0530

    submit

---
+++ Day 10/AnonymousMethodParameter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethodParameter
+{
+    public delegate void Print(int value);
+    class Program
+    {
+        public static void PrintHelperMethod(Print printDel, int val)
+        {
+            val += 10;
+            printDel(val);
+        }
+        static void Main(string[] args)
+        {
+            PrintHelperMethod(delegate (int val)
+            {
+                Console.WriteLine("Anonymous method: {0}", val);
+            }, 100);
+        }
+    }
+}
