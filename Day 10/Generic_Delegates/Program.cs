e9f5e0d9b9a40ba5834ab0d77572540308ede6c2
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:06:08 +0530

    submit

---
+++ Day 10/Generic_Delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDelegates
+{
+    class Program
+    {
+        public static bool IsDate(string date)
+        {
+            DateTime dt;
+            return DateTime.TryParse(date, out dt);
+        }
+        public static int Multi(int a, int b)
+        {
+            return a * b;
+        }
+        public static void sum(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine(sum);
+        }
+        public static void add(string fname, string lname)
+        {
+            string fullname = fname + " " + lname;
+            Console.WriteLine(fullname);
+        }
+        static void Main(string[] args)
+        {
+            Action<int, int> a = new Action<int, int>(Program.sum);
+            a(15, 10);
+
+            Action<string, string> b = new Action<string, string>(Program.add);
+            b("chirag", "bedi");
+
+            Func<int, int, int> fs = new Func<int, int, int>(Program.Multi);
+            Console.WriteLine(fs(10, 23));
+
+            Predicate<string> date = new Predicate<string>(Program.IsDate);
+            if (date("10-26-2022"))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            Console.ReadKey();
+        }
+    }
+}
