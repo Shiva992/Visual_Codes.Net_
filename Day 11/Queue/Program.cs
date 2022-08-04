36bfc3b6646e5c28f3b5e30537f5e7bf80c4a22c
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:18:40 +0530

    submit

---
+++ Day 11/Queue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericQueue
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Queue<string> numbers = new Queue<string>();
+            numbers.Enqueue("one");
+            numbers.Enqueue("two");
+            numbers.Enqueue("three");
+            numbers.Enqueue("four");
+            numbers.Enqueue("five");
+
+            foreach (string number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("\nDequeuing '{0}'", numbers.Dequeue());
+            Console.WriteLine("Peek of queue now: {0}",
+                numbers.Peek());
+            Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
+
+
+
+
+
+
+
+        }
+    }
+}
