420f064b8d36449afebe108ac406ce63c104946f
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 23:39:08 +0530

    submit

---
+++ Day 7/ExtensionsMethodDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsDemo
+{
+    public static class StringExtension
+    {
+        public static int GetWordCount(this string inputstring)
+        {
+            if (!string.IsNullOrEmpty(inputstring))
+            {
+                string[] strArray = inputstring.Split(' ');
+                return strArray.Count();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static int Addition(int a, int b)
+        {
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myword = "Welcome to Elearning tutors by GLobal Logic TRainings";
+            int wordCount = myword.GetWordCount();
+
+            //StringExtension.GetWordCount(myword);
+            Console.WriteLine("string : " + myword);
+            Console.WriteLine("Count : " + wordCount);
+            Console.ReadLine();
+        }
+    }
+}
