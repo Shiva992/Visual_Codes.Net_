06ad3b6f322ba357d394788eba3e3ce50df819da
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:11:46 +0530

    submit

---
+++ Day 10/EventsHandleExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace EventHandlingExample
+{
+    public delegate void DelEventHandler();
+    class Program : Form
+    {
+        public event DelEventHandler add;
+
+        public Program()
+        {
+            Button btn = new Button();
+            btn.Parent = this;
+            btn.Text = "Click Me";
+            btn.Location = new Point(100, 100);
+
+            btn.Click += new EventHandler(onClick);
+            add += new DelEventHandler(Initiate);
+            add();
+        }
+
+        public void Initiate()
+        {
+            Console.WriteLine("Event Initiated");
+        }
+
+        public void onClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button Clicked");
+        }
+
+        static void Main(string[] args)
+        {
+            Application.Run(new Program());
+            Console.ReadLine();
+        }
+    }
+}
