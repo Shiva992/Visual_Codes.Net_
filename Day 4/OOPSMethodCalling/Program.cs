d3fb589a09da78f535ae9474e5ee628b37788268
Code with Shiva <shiva.1@globallogic.com>
2022-08-04 21:54:34 +0530

    submit

---
+++ Day 4/OOPSMethodCalling/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsconcept
+{
+    internal class Processor
+    {
+        int abc, _empid;
+        string xyz, _eaddress;
+        public int empid
+        {
+            set { _empid = value; }
+            get
+            {
+                return _empid;
+            }
+
+        }
+        public int ABC
+        {
+
+            set
+            {
+                abc = value;
+            }
+            get
+            {
+                return abc;
+            }
+        }
+        public string EFG
+        {
+
+            set
+            {
+                xyz = value;
+            }
+            get
+            {
+                return xyz;
+            }
+        }
+        public string eaddress
+        {
+            set { _eaddress = value; }
+            get { return _eaddress; }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processor p = new Processor();
+            p.ABC = 51;
+            p.EFG = "name";
+            p.eaddress = "bly";
+            p.empid = 101;
+            Console.WriteLine(p.ABC);
+            Console.WriteLine(p.EFG);
+            Console.WriteLine(p.eaddress);
+            Console.WriteLine(p.empid);
+
+
+
+        }
+    }
+}
