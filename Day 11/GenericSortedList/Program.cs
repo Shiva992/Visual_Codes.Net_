e6bdd53253d90808c6d3e1330ed247183c54cde4
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 00:20:22 +0530

    submit

---
+++ Day 11/GenericSortedList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating 
+            //SortedList List = new SortedList(); // non generic sorted list
+            SortedList<string, string> subjectTeacher = new SortedList<string, string>();
+
+            //Adding
+            subjectTeacher.Add("Arihant", "Maths");
+            subjectTeacher.Add("Amit", "Science");
+            subjectTeacher.Add("Rishabh", "English");
+            subjectTeacher.Add("Chirag", "Computer Sci");
+
+
+            //Display
+            foreach (var item in subjectTeacher)
+            {
+                Console.WriteLine(item.Key + ", the subject taught by this teacher is " + item.Value);
+            }
+
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Index : ");
+
+            //Index
+            subjectTeacher["Amit"] = "Hindi";
+            Console.WriteLine("Checking the value for Amit " + subjectTeacher["Amit"]);
+
+            //Remove
+            Console.WriteLine("=============================");
+            Console.WriteLine("After Removing");
+            subjectTeacher.Remove("Chirag");
+
+            //Count
+            Console.WriteLine("Count is: " + subjectTeacher.Count);
+
+
+            Console.WriteLine("=======================");
+            //Contains
+            Console.WriteLine("Checking the value for subject Maths " + subjectTeacher.ContainsValue("Maths"));
+            Console.ReadKey();
+
+        }
+    }
+}
