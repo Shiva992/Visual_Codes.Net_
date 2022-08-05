55ad9e84bec8c0cead0b8c7fa7dcf0c5aec49676
Code with Shiva <shiva.1@globallogic.com>
2022-08-05 10:32:13 +0530

    submit

---
+++ day 29 Task/Task exception handling/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_exception_handling
+{
+    class Account
+    {
+        public Account(string name, int balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+        public string Name { get; private set; }
+        public int Balance { get; private set; }
+
+        public void Debit(int amount)
+        {
+            if (Balance < amount) throw new Exception(Name, Balance, amount);
+            Balance = Balance - amount;
+        }
+
+        public void Credit(int amount) => Balance = amount + Balance;
+    }
+}
---
+++ day 29 Task/Task exception handling/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Task_exception_handling;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var account = new Account("Shiva", 150);
+            try
+            {
+                account.Debit(200);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception \nException Message: " + ex.Message);
+                Console.WriteLine("Account Balance: " + ex.AccountBalance);
+                Console.WriteLine("Transaction Amount: " + ex.TransactionAmount);
+            }
+
+            Console.Read();
+        }
+    }
+}
