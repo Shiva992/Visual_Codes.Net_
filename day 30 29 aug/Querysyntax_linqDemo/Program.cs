c95092d5daa53b45c0db0ce71e41a6b0ed7c4249
Code with Shiva <shiva.1@globallogic.com>
2022-08-08 14:34:42 +0530

    Linq_Demo

---
+++ day 30 29 aug/Querysyntax_linqDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Querysyntax_linqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+            //LINQ Query using Query Syntax
+            var QuerySyntax = from obj in integerList
+                              where obj > 2
+                              select obj;
+            //Executive
+            foreach (var item in QuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
