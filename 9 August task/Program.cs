5034e88622d15ed3a64285ca2f73f7d6f12bd54d
Code with Shiva <shiva.1@globallogic.com>
2022-08-08 09:21:44 +0530

    submit

---
+++ 9 August task/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_August_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shiva");
+        }
+    }
+}
