124848e57225b8b160befa6aaf7bb7971a24e4c0
Code with Shiva <shiva.1@globallogic.com>
2022-08-09 13:07:31 +0530

    SortDemo

---
+++ Day 30 ExpressionsTrees 9 AUGUST/Linq_XML_SORT_DEMO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Xml_demo
+{
+    class Class1
+    {
+
+
+        static void Main(string[] args)
+        {
+            XElement xelement = XElement.Load(@"D:\.Net_Training\Mynewrepositry\VisualCodes _.Net_training\Visualcodes\Day 30 ExpressionsTrees 9 AUGUST");
+
+            var dict = (from element in xelement.Descendants("")
+                        let name = (string)element.Attribute("Name")
+                        select new
+                        {
+                            CusiID = element.Attribute("CustId").Value,
+                            CustName = name
+
+                        })
+        .ToDictionary(c => c.CustID, c => c.CustName);
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
