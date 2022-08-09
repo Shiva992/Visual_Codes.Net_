c02d39873db418467d52b4d7bbec708510a48d5f
Code with Shiva <shiva.1@globallogic.com>
2022-08-09 13:03:10 +0530

    XML

---
+++ Day 30 ExpressionsTrees 9 AUGUST/Linq_Xml demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace Linq_Xml_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = @"<?xml version = ""1.0""?>
+                <country name = ""India"">
+                <Capital>NewDelhi</capital>
+                </Country>";
+            //Preserve whitespaces
+
+            XDocument XDoc = XDocument.Parse(str, LoadOptions.PreserveWhitespace);
+            Console.WriteLine(XDoc);
+            Console.ReadLine();
+        }
+    }
+}
