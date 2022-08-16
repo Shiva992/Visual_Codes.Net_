9f248ebe79a72fef30b8c8c2675f9e6f071099d1
Code with Shiva <shiva.1@globallogic.com>
2022-08-16 11:40:28 +0530

    Day 23 Task _16 august_ Layers

---
+++ 16aug/Interfaces/IBusinessLogicLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement
+{
+    public interface IBusinessLogicLayer
+    {
+        bool UpdateProductPrice(int id, int price);
+        void DisplayAllProduct();
+        void DisplayAllCategory();
+        void DisplayProductBasedOnProductID(int id);
+        bool DeleteProduct(int id);
+    }
+}
