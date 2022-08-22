e6a0f7052d13e196d270f0d62e02e8e13928f113
Code with Shiva <shiva.1@globallogic.com>
2022-08-22 12:20:27 +0530

    Linq_OPERATION

---
+++ LinqOperation/LinqOperation/Form1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form1.aspx.cs" Inherits="LinqOperation.Form1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+    <asp:ListBox ID="ListBox1" runat="server" SelectionMode="Multiple">
+        <asp:ListItem Text="One" Value="One" />
+        <asp:ListItem Text="Two" Value="Two" />
+        <asp:ListItem Text="Three" Value="Three" />
+        <asp:ListItem Text="Four" Value="Four" />
+        <asp:ListItem Text="Five" Value="Five" />
+        <asp:ListItem Text="Six" Value="Six" />
+        <asp:ListItem Text="Seven" Value="Seven" />
+    </asp:ListBox>
+
+    <asp:ListBox ID="ListBox2" runat="server" SelectionMode="Multiple">
+        <asp:ListItem Text="One" Value="One" />
+        <asp:ListItem Text="Two" Value="Two" />
+        <asp:ListItem Text="Three" Value="Three" />
+        <asp:ListItem Text="Four" Value="Four" />
+        <asp:ListItem Text="Five" Value="Five" />
+        <asp:ListItem Text="Six" Value="Six" />
+        <asp:ListItem Text="Seven" Value="Seven" />
+    </asp:ListBox>
+
+    <br />
+    <asp:Button ID="Button2" runat="server" Text="Get Selected Items"
+        onclick="Button2_Click" />
+</div>
+    </form>
+</body>
+</html>
---
+++ LinqOperation/LinqOperation/Form1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinqOperation
+{
+    public partial class Form1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var distinct = from p in ListBox1.Items.OfType<ListItem>()
+                        .Concat(ListBox2.Items.OfType<ListItem>())
+                        .Where(o => o.Selected)
+                        .Distinct()
+                           select new
+                           {
+                               Text = p.Text
+                           };
+
+            foreach (var item in distinct)
+            {
+                Response.Write(item.Text + "<br/>");
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var list1 = from p in ListBox1.Items.OfType<ListItem>()
+                        .Where(o => o.Selected)
+                        select new
+                        {
+                            Text = p.Text
+                        };
+
+            var list2 = from p in ListBox2.Items.OfType<ListItem>()
+                    .Where(o => o.Selected)
+                        select new
+                        {
+                            Text = p.Text
+                        };
+
+            var unique = list1.Union(list2);
+
+            foreach (var item in unique)
+            {
+                Response.Write(item.Text + "<br/>");
+            }
+        }
+    }
+}
