d1a6fd751e8e148f3f38db6565bad6d8404b370f
Code with Shiva <shiva.1@globallogic.com>
2022-08-24 13:19:43 +0530

    PartialViewDemo

---
+++ MVC/Mvc Day1/ActionResultTypes/Controllers/ResultTypesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActionResultTypes.Controllers
+{
+    public class ResultTypesController
+    {
+        public ViewResult ShowEmployee()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeID = 1,
+                FirstName = "Antony",
+                LastName = "Paul",
+                Location = "Bangalore",
+                Email = "Paul@gmail.com"
+            };
+            return View(employee);
+        }
+        public ContentResult ContentResultTest()
+        {
+            return Content("<b>Hello from GL<b>", "text/html", System.Text.Encoding.UTF8);
+        }
+        public RedirectResult RedirectResultTest()
+        {
+            return Redirect("https://google.com");
+        }
+        public FileResult FileResultTest()
+        {
+            return File("/downloads/sample.pdf", "application.pdf");
+
+        }
+        public StatusCodeResult StatusCodeResultTest()
+        {
+            return StatusCode(200);
+        }
+    }
+}
---
+++ MVC/Mvc Day1/PartialViewDemo/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartialViewDemo.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
