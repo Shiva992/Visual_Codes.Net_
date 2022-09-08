efba7b73df29496b0ddc80986d5d469d8c32bfc9
Code with Shiva <shiva.1@globallogic.com>
2022-09-08 10:07:02 +0530

    Delegates 3

---
+++ Delegates/ConsoleApp1/MulticastDelegates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MulticastDelegates
+    {
+        public delegate void MyDelegate(int a, int b);
+        static void Main(string[]args)
+        {
+            MyDelegate m = Add;
+            m = m + Multiply;
+            m = m + Subtract;
+            m(200, 50);
+            Console.WriteLine("--------------------");
+            m = m - Multiply;
+            m.Invoke(300, 100);
+            Console.ReadKey();
+        }
+        public static void Add(int x, int y)
+        {
+            Console.WriteLine("Addition= " + (x + y));
+        }
+        public static void Multiply(int x, int y)
+        {
+            Console.WriteLine("Multiplication=" + (x * y));
+        }
+        public static void Subtract(int x, int y)
+        {
+            Console.WriteLine("Subtraction=" + (x - y));
+        }
+    }
+}
