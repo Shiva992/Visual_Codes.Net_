df976ef29abbcad7f897a015506208a675e2d88d
Code with Shiva <shiva.1@globallogic.com>
2022-09-08 09:44:03 +0530

    Delegates 2

---
+++ Delegates/ConsoleApp1/DelegateParameter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DelegateParameter
+    {
+        public delegate void MyDelegate(string str);
+        static void Main(string[]args)
+        {
+            MyDelegate m = print;
+            InvokeMyDelegate(m);
+            Console.ReadKey();
+        }
+        public static void InvokeMyDelegate(MyDelegate md)
+        {
+            md("This is delegate which is passed as a method parameter");
+        }
+        public static void print(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
+}
