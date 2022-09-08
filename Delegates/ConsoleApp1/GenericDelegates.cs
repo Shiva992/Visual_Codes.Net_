6bfa5aa22c1c4349150e94713479cea8e64937cd
Code with Shiva <shiva.1@globallogic.com>
2022-09-08 10:30:03 +0530

    GenericDelegates4

---
+++ Delegates/ConsoleApp1/GenericDelegates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class GenericDelegates
+    {
+        public delegate T MyGenericDelegate<T>(T a, T b);
+        static void Main(string[] args)
+        {
+            MyGenericDelegate<int> dAdd = Add;
+            Console.WriteLine(dAdd(10, 20));
+
+            MyGenericDelegate<string> dstring = AddString;
+            Console.WriteLine(dstring("Hi" , "Generic Delegates"));
+            Console.ReadKey();
+        }
+            public static int Add(int a , int b)
+        {
+            return a + b;
+        }
+        public static string AddString(string s1 , string s2)
+        {
+            return s1 + s2;
+        }
+        }
+    }
+
